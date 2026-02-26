18fdf0fdb2e32ef74a2e4ea6b5f90feba0a8473c
dremota234 <159703089+dremota234@users.noreply.github.com>
2026-02-26 16:51:52 +0300

    test script
    был добавлен скрипт Test который просто вызывает создаёт объекты из всех прикрепленных к нему ассетов

---
+++ Test.cs
@@ -0,0 +1,20 @@
+﻿using Assets.CreateObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.ObjectManager
+{
+    public class Test : MonoBehaviour
+    {
+        [SerializeField] private Object3DAsset[] object3DAssets;
+        private void Start()
+        {
+            foreach (Object3DAsset object3 in object3DAssets)
+            {
+                object3.CreateObject();
+            }
+        }       
+    }
+}
